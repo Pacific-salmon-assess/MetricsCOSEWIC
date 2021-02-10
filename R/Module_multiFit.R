2fad1eae0e1628ae6ed0590d66f33b5c520bd56e
SOLV-Code <30556192+SOLV-Code@users.noreply.github.com>
2021-02-10 00:05:22 -0800

    fn tweaks

--- R/Module_multiFit.R
+++ R/Module_multiFit.R
@@ -9,21 +9,24 @@
 
 multiFit  <- function(data.df, window.df, plot.file =  "PercChange_Plots.pdf"){
 
-out.df <- data.frame() # as per https://stackoverflow.com/a/32342704
+# set up empty data frame (as per https://stackoverflow.com/a/32342704)
+out.df <- data.frame() 
 
+# start the timer
 loop.start <- proc.time()
 
-
+# open the pdf file
 pdf(plot.file ,onefile=TRUE,height=8.5, width = 11)
 
+# start the loop
 for(du.do in sort(unique(data.df$DU)) ){
 
 print(paste("starting",du.do, "--------------------------------"))
 
 
-
 layout(matrix(c(1,1,2,3),ncol=2,byrow=TRUE))
 
+# extract the data for the current DU
 df.use <- data.df %>% dplyr::filter(DU == du.do) %>%  select(Year,Abd)
 last.yr <- max(df.use$Year)
 
@@ -36,7 +39,7 @@ if(sum(!is.na(df.use$Abd)) > 6 ){
 # TEMPORARY PATCH (erplace 0 b/c log transform inside of fn call below)
 df.use <- df.use  %>% mutate(Abd = recode(Abd, "0" = 0.1))
 
-
+# Do the calculations
 fit.out <- comparePercChange(du.label = du.do,
                               du.df = df.use,
                               yrs.window = window.df  %>% dplyr::filter(DU == du.do) %>%  select(Window) %>% unlist,
@@ -59,7 +62,7 @@ title(main = du.do, outer=TRUE,line=-2,col.main = "darkblue",cex.main=1.7)
 dev.off()
 
 
-
+# generate summary tables
 summary.out <- out.df %>% select(DU,Year,Var, pchange) %>% dplyr::filter(grepl("MLE", Var ) | grepl("Med", Var )) %>%
    mutate(pchange = round(pchange,1)) %>%
    pivot_wider(id_cols = c(DU,Year),names_from = Var, values_from = pchange ) %>%
