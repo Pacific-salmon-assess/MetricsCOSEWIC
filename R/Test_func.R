841fb831020a5ec19411aa3b6f796a1a277caee5
Kitching <Tor.Kitching@dfo-mpo.gc.ca>
2023-07-31 15:48:02 -0700

    Created testing function for practice
    Practicing function development workflow - Tor

---
+++ R/Test_func.R
@@ -0,0 +1,11 @@
+#' Test function for working on Stan inclusion
+#' params list for function params
+#' Testing out load_all() devtools and roxygenise
+#' @param arg1 A value to return
+#' @examples
+#' testing_fun("sample") # returns "sample"
+#' @export
+
+testing_fun <- function(arg1 = "Change me") {
+  print(arg1)
+}
